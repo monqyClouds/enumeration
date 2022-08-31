5fba6f36087844847000096f33e47ad79b112a53
monqyClouds <marcellusarthurs@gmail.com>
2022-08-31 15:20:03 -0700

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        
+    }
+}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // }
+}
